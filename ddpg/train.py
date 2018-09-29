bb1784e068df98c9fcbe387c62d0f3fb851907c4
Slavo Matasovsky <slavo.matas@gmail.com>
2018-09-29 15:47:58 +0200

    Updated ddpg train method

---
+++ ddpg/train.py
@@ -0,0 +1,96 @@
+import os
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+from collections import deque
+from unityagents import UnityEnvironment
+
+import sys
+sys.path.append("../")
+
+from agent import Agent
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+env = UnityEnvironment(file_name="../Reacher_Linux/Reacher.x86_64", no_graphics=True)
+
+# get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+
+# reset the environment
+env_info = env.reset(train_mode=True)[brain_name]
+
+# number of agents in the environment
+print('Number of agents:', len(env_info.agents))
+
+# number of actions
+action_size = brain.vector_action_space_size
+print('Number of actions:', action_size)
+
+# examine the state space
+state = env_info.vector_observations[0]
+print('States look like:', state)
+state_size = len(state)
+print('States have length:', state_size)
+
+state = env_info.vector_observations[0]  # get the current state
+score = 0  # initialize the score
+
+# Instantiate DQN Agent
+agent = Agent(state_size=state_size, action_size=action_size, random_seed=0)
+
+
+def ddpg(n_episodes=1000, max_t=300, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+    """Deep Deterministic Policy Gradient
+
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+    scores = []  # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start  # initialize epsilon
+    for i_episode in range(1, n_episodes + 1):
+        env_info = env.reset(train_mode=True)[brain_name]
+        state = env_info.vector_observations[0]
+        score = 0
+        for t in range(max_t):
+            action = agent.act(state, eps)
+            env_info = env.step(action)[brain_name]
+            next_state = env_info.vector_observations[0]  # get the next state
+            reward = env_info.rewards[0]  # get the reward
+            done = env_info.local_done[0]  # see if episode has finished
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                break
+        scores_window.append(score)  # save most recent score
+        scores.append(score)  # save most recent score
+        eps = max(eps_end, eps_decay * eps)  # decrease epsilon
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window) >= 13.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode - 100,
+                                                                                         np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), "../checkpoints/checkpoint.pth")
+            break
+    return scores
+
+
+scores = ddpg()
+
+# plot the scores
+fig = plt.figure()
+#ax = fig.add_subplot(111)
+plt.plot(np.arange(len(scores)), scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
